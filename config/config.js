9f441e023c0cee40d7f4b1ed57872ca611ae06df
KaungSetLin <kaungsetlin3013@gmail.com>
2026-07-19 02:22:31 +0900

    Change NOTESYNC_CLIENT_ID in config.js
    Updated the NOTESYNC_CLIENT_ID to the new value.

--- config/config.js
+++ config/config.js
@@ -5,4 +5,4 @@
 //   → APIs & Services → Credentials → OAuth 2.0 Client ID
 // ─────────────────────────────────────────
 
-window.NOTESYNC_CLIENT_ID = 'YOUR_CLIENT_ID_HERE.apps.googleusercontent.com';
+window.NOTESYNC_CLIENT_ID = '778480488304-d734v7aai7lmo65tn1j8146pq0vldq69.apps.googleusercontent.com';
